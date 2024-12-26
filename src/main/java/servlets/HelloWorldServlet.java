ce0eb26f77f09f5974509e6173e9816104bde0f5
ThibaultGM78 <thibault.garciam@gmail.com>
2024-12-26 11:44:33 +0100

    1st commit

---
+++ src/main/java/servlets/HelloWorldServlet.java
@@ -0,0 +1,48 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+@WebServlet("/helloWorld") 
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorldServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html"); 
+		
+		PrintWriter out = response.getWriter(); out.println("<HTML>");
+		out.println("<HEAD><TITLE> Titre </TITLE></HEAD>"); out.println("<BODY>");
+		out.println("Hello World, this is my first servlet");
+		out.println("</BODY>");
+		out.println("</HTML>"); out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
